30b9d6044a5731491c267a49928bbda754fb9a2f
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-02 20:55:23 +0800

    dsds

--- resources/views/filament/pages/fitness-subscription.blade.php
+++ resources/views/filament/pages/fitness-subscription.blade.php
@@ -4,8 +4,24 @@
     @livewire('member.pricing')
     @livewireScripts()
     <style>
-        .fi-header{
+        .fi-header {
             display: none !important;
         }
     </style>
+    <script>
+        const html = document.documentElement;
+
+        // Function to check for class every 1 second
+        setInterval(() => {
+            const elem = document.querySelector(".fi"); // Target element with class 'fi'
+            if (!elem) return;
+
+            if (elem.classList.contains("dark")) {
+                html.classList.add("dark"); // Activate Tailwind dark mode
+            } else {
+                html.classList.remove("dark"); // Deactivate dark mode
+            }
+        }, 1000); // 1000ms = 1 sec
+    </script>
+
 </x-filament-panels::page>
