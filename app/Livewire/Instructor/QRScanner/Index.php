8eec10b7a5db3cc9601ddc2440252f68474e21cb
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-02 22:07:39 +0800

    dsds

--- app/Livewire/Instructor/QRScanner/Index.php
+++ app/Livewire/Instructor/QRScanner/Index.php
@@ -3,16 +3,11 @@
 namespace App\Livewire\Instructor\QRScanner;
 
 use Livewire\Component;
-use App\Models\FitnessOffer;
-use Illuminate\Support\Facades\Auth;
 
 class Index extends Component
 {
     public function render()
     {
-        $fitnessOffers = FitnessOffer::where('id', Auth::user()->subscriptions->fitness_offer_id)->get();
-        return view('livewire.instructor.q-r-scanner.index', [
-            'fitnessOffers' => $fitnessOffers,
-        ]);
+        return view('livewire.instructor.q-r-scanner.index');
     }
 }
--- app/Livewire/Member/QRCode/Index.php
+++ app/Livewire/Member/QRCode/Index.php
@@ -3,11 +3,16 @@
 namespace App\Livewire\Member\QRCode;
 
 use Livewire\Component;
+use App\Models\FitnessOffer;
+use Illuminate\Support\Facades\Auth;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.member.q-r-code.index');
+        $fitnessOffers = FitnessOffer::where('id', Auth::user()->subscriptions->fitness_offer_id)->get();
+        return view('livewire.member.q-r-code.index', [
+            'fitnessOffers' => $fitnessOffers,
+        ]);
     }
 }
