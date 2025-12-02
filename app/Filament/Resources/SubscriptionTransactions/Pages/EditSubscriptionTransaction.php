3331609282049e84864d395aa95371f6e5204df3
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-02 13:39:45 +0800

    ds

--- app/Filament/Resources/SubscriptionTransactions/Pages/EditSubscriptionTransaction.php
+++ app/Filament/Resources/SubscriptionTransactions/Pages/EditSubscriptionTransaction.php
@@ -15,7 +15,7 @@ class EditSubscriptionTransaction extends EditRecord
     {
         return [
             ViewAction::make(),
-            DeleteAction::make(),
+            // DeleteAction::make(),
         ];
     }
 }
