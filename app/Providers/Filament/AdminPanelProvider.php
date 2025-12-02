3a8b91ddec24c95ca2d6b45c1d4b0439ab221969
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-02 18:12:12 +0800

    dsds

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,21 +11,16 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Andreia\FilamentUiSwitcher\FilamentUiSwitcherPlugin;
-use BinaryBuilds\CommandRunner\CommandRunnerPlugin;
 use Filament\Support\Enums\Width;
-use daacreators\CreatorsTicketing\TicketingPlugin; // Add this line
+use daacreators\CreatorsTicketing\TicketingPlugin; 
 use Filament\Actions\Action;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Middleware\AdminRoleMiddleware;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -49,13 +44,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                // AccountWidget::class,
-                // FilamentInfoWidget::class,
             ])
             ->plugins([
                 FilamentShieldPlugin::make(),
                 TicketingPlugin::make(),
-                // CommandRunnerPlugin::make(),
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -82,7 +74,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-                AdminRoleMiddleware::class,
             ]);
     }
 }
