42192d2f31c83046688464c8bbcf656c4f00f9b0
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-02 22:10:53 +0800

    dsds

--- app/Livewire/Member/QRCode/Index.php
+++ app/Livewire/Member/QRCode/Index.php
@@ -10,7 +10,9 @@ class Index extends Component
 {
     public function render()
     {
-        $fitnessOffers = FitnessOffer::where('id', Auth::user()->subscriptions['fitness_offer_id'])->get();
+        $activeSubscription = Auth::user()->subscriptions()->where('status', 'active')->first();
+        $fitness_offer_id = $activeSubscription ? $activeSubscription->fitness_offer_id : null;
+        $fitnessOffers = $fitness_offer_id ? FitnessOffer::where('id', $fitness_offer_id)->get() : collect();
         return view('livewire.member.q-r-code.index', [
             'fitnessOffers' => $fitnessOffers,
         ]);
