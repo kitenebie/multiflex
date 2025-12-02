7d3c83825252b1b5f94beeb28bf53b368273e1c7
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-02 20:36:29 +0800

    dsd

---
+++ app/Livewire/Member/Pricing.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Member;
+
+use Livewire\Component;
+
+class Pricing extends Component
+{
+    public function render()
+    {
+        return view('livewire.member.pricing');
+    }
+}
--- resources/views/filament/pages/fitness-subscription.blade.php
+++ resources/views/filament/pages/fitness-subscription.blade.php
@@ -1,3 +1,6 @@
 <x-filament-panels::page>
     {{-- Page content --}}
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    @livewire('member.pricing')
+    @livewireScripts()
 </x-filament-panels::page>
