1d0c7615071b612cc79d631d17da4ccb71cd5d07
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-16 15:09:11 +0800

    dsds

--- app/Filament/Resources/Payslips/Schemas/PayslipForm.php
+++ app/Filament/Resources/Payslips/Schemas/PayslipForm.php
@@ -2,11 +2,14 @@
 
 namespace App\Filament\Resources\Payslips\Schemas;
 
+use App\Models\User;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
+
 
 class PayslipForm
 {
@@ -16,6 +19,14 @@ class PayslipForm
             ->components([
                 Select::make('user_id')
                     ->relationship('user', 'name')
+                    ->label('Coach')
+                    ->searchable()
+                    ->reactive()
+                    ->afterStateUpdated(function ($state, Set $set) {
+                        logger('Selected user ID:', [$state]); // DEBUG
+                        $salary = User::whereKey($state)->value('daily_basic_salary');
+                        $set('basic_salary', $salary ?? 0);
+                    })
                     ->required(),
                 DatePicker::make('period_start')
                     ->required(),
