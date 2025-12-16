dd63fedd5cf6eb8ff96fa4077a534a68e44ac79f
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-16 20:14:16 +0800

    dsdsd

--- app/Http/Controllers/PaySlipController.php
+++ app/Http/Controllers/PaySlipController.php
@@ -12,7 +12,7 @@ class PaySlipController extends Controller
         $user = Auth::user();
         dd($user);
         $PaySlip = \App\Models\Payslip::findOrFail($PaySlipId);
-        if($PaySlip->user_id !== $user->id || $user->roles()->where('name', 'admin')->exists()){
+        if($PaySlip->user_id !== $user->id || $user->role == 'admin'){
             abort(403, 'Unauthorized action.');
         }
         return view('filament.pages.payslip-print', compact('PaySlip'));
