fc5d66aab1090ad579e8e1c62bf5d0871e616a60
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-16 15:04:14 +0800

    dsds

--- app/Models/Payslip.php
+++ app/Models/Payslip.php
@@ -38,6 +38,6 @@ class Payslip extends Model
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->role('coach');
     }
 }
