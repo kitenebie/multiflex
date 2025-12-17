02a9c4861d64b612375b0358dfeaa8f06506a2f9
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-17 09:09:05 +0800

    dds

--- app/Filament/Resources/Payslips/Tables/PayslipsTable.php
+++ app/Filament/Resources/Payslips/Tables/PayslipsTable.php
@@ -20,7 +20,7 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Toggle;
-use Filament\Forms\Components\Grid;
+use Filament\Schemas\Components\Grid;
 
 class PayslipsTable
 {
